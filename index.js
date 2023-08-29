873476911f4aa55e98e30777e9505ce61b0a1e6d
Teddy <teddy.klay@gmail.com>
2023-08-29 14:26:09 +0300

    more router fixes...

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const main = document.getElementById('main');
 const homeButton = document.getElementsByClassName('goHome')[0];
 const addButton = document.getElementsByClassName('addProduct')[0];
 
-homeButton.addEventListener('click', () => ROUTER.goHome(main));
-addButton.addEventListener('click', () => ROUTER.editPage(main,null));
+homeButton.addEventListener('click', () => ROUTER.goHome());
+addButton.addEventListener('click', () => ROUTER.editPage(null));
 
 const loginButton = document.getElementById('login');
-loginButton.addEventListener('click', () => ROUTER.login(main));
+loginButton.addEventListener('click', () => ROUTER.login());
 
 const signUpButton = document.getElementById('signup');
-signUpButton.addEventListener('click', () => ROUTER.signUp(main));
+signUpButton.addEventListener('click', () => ROUTER.signUp());
 
 async function start() {
-   await ROUTER.productsPage(main);
+   await ROUTER.productsPage();
 }
 start();
